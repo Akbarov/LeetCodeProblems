7188ddcb29de1fa30a3e3f640d824c5a983add61
zohidjonakbarov <zohidjonakbarov@gmail.com>
2022-03-19 08:13:59 +0500

    middle problems solved

---
+++ src/main/kotlin/problems/SmallestSubsequenceDistinctCharacters_1081.kt
@@ -0,0 +1,33 @@
+package problems
+
+import java.util.*
+
+/**
+Creator: Zohidjon Akbarov
+ */
+fun main() {
+    println(smallestSubsequence("bcabc"))
+}
+private fun smallestSubsequence(s: String): String {
+    if (s.length <= 1) return s
+    val visited = BooleanArray(26)
+    val stack = Stack<Char>()
+    val lastIndex = IntArray(26)
+    for ((index, c) in s.withIndex()) {
+        lastIndex[c - 'a'] = index
+    }
+
+    for ((index, c) in s.withIndex()) {
+        if (visited[c - 'a']) continue
+        while (stack.isNotEmpty() && stack.peek() > c && index < lastIndex[stack.peek() - 'a']) {
+            visited[stack.pop() - 'a'] = false
+        }
+        stack.push(c)
+        visited[c - 'a'] = true
+    }
+    val stringBuilder = StringBuilder()
+    while (stack.isNotEmpty()) {
+        stringBuilder.append(stack.pop())
+    }
+    return stringBuilder.reverse().toString()
+}
